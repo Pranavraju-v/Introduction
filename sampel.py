f6550924a5231b7f9b13b2cbe8b53bf1eb971755
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-02-27 20:11:33 +0100

    Add files via upload

---
+++ sampel.py
@@ -0,0 +1,11 @@
+print(34)
+("welcome")
+number=34
+print(number)
+name="pranav"
+print(name)
+print("my name is:",name)
+print("the value =",number)
+
+name1=input("what is your name\n")
+print("hello" ,name1,end= "bye")
