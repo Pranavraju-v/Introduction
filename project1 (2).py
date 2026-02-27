70838e193806bec567f80319e8cd869442a883a1
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-02-27 22:02:21 +0100

    Add files via upload

---
+++ project1 (2).py
@@ -0,0 +1,12 @@
+
+max_bday = "July 7"
+pranav_bday = "October 04"
+tony_bday = "January 28"
+kunal_bday = "August 28"
+rahul_bday = "December 25"
+
+print("Max's birthday is on " + max_bday)
+print("Pranav's birthday is on " + pranav_bday)
+print("Tony's birthday is on " + tony_bday)
+print("Kunal's birthday is on " + kunal_bday)
+print("Rahul's birthday is on " + rahul_bday)
