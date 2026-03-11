3df3ada07f3024da320982d75ca3c8e520bd1399
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-03-11 20:26:17 +0100

    Add files via upload

---
+++ Percentage_ Calculation.py
@@ -0,0 +1,11 @@
+print("Please enter makes in each subject")
+
+math=int(input("Math score:"))
+science=int(input("Science score:"))
+hindi=int(input("Hindi score:"))
+english=int(input("Englich score:"))
+
+print("Your percentage in math is",math,("%"))
+print("Your percentage in math is",science,("%"))
+print("Your percentage in math is",hindi,("%"))
+print("Your percentage in math is",english,("%"))
