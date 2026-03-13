55d5828c87608a6e8ac9d51c428ab1ab1142e0a7
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-03-13 20:48:23 +0100

    Add files via upload

---
+++ Greater_or_smaller_activity.py
@@ -0,0 +1,8 @@
+num=int(input("Enter number: "))
+if num>15:
+    print("Number is greater than 15")
+    print("This is inside if")
+else:
+    print("Number is smaller than 15")
+    print("This is inside else")
+print("This inside niether if or else")
