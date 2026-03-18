4e244f1f09b6e41b3daa7ea80d04b18248b82a85
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-03-18 19:54:46 +0100

    Add files via upload

---
+++ not_equals.py
@@ -0,0 +1,22 @@
+a=12
+b=23
+c=52
+
+print(a!=b)
+print(c!=b)
+
+A="Python"
+B="coding"
+
+print(A,"and", B ,"are not the same")
+
+a=4
+b=2
+if(a==1) != (b==5):
+  print("Hello")
+
+num=int(input("enter a number: "))
+if num%2 != 0:
+   print("The number is odd")
+else:
+   print("the number is even")
