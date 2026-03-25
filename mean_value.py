c1d9c1c1578ed1ee8743adfaaba35d8d658b119c
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-03-25 20:00:35 +0100

    Add files via upload

---
+++ mean_value.py
@@ -0,0 +1,9 @@
+mean1=38
+wrongnum=36
+right=56
+total_number=40
+sum=mean1*total_number
+print("The sum of the 40 numbers is : ", sum)
+num2=sum-((wrongnum)-(right))
+right_mean=num2/total_number
+print("The right mean is", right_mean)
