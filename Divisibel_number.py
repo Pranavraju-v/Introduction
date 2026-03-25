ec6efbe6f479d02546c6103b81f79736299e00ec
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-03-25 19:40:21 +0100

    Add files via upload

---
+++ Divisibel_number.py
@@ -0,0 +1,6 @@
+num=int(input("Enter the first number: "))
+num1=int(input("Enter second number: "))
+if num//num1==1:
+   print("The numbers are divisibel by themselves")
+else:
+   print("Thay are not divisibel by themselves")
