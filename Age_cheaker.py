7714f29655859a94b9567d1a49943fff769a921d
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-04-02 12:50:03 +0200

    Add files via upload

---
+++ Age_cheaker.py
@@ -0,0 +1,7 @@
+age=int(input("Enter yor age: "))
+if age <= 20:
+    print("You are allowed to go to 10th grade.")
+    if age >= 10:
+       print("You are allowed to go to 10th grade.") 
+else:
+    print("Sorry not allowed to join 10th grade")
