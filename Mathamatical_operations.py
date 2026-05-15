2eb22403d9c1d78dd80f774c6928b16a0707dceb
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-05-15 20:36:46 +0200

    Add files via upload

---
+++ Mathamatical_operations.py
@@ -0,0 +1,7 @@
+import math
+print("The floor and ceil value of 23.45 is:",str(math.ceil(23.45)) +  "," + str(math.floor(23.45)))
+x=10
+y=-10
+print("The value of x after coping the sign of y is",str(math.copysign(x,y)))
+print("The absalout value of -96 and 56 is",str(math.fabs(-96)) + "," + str(math.fabs(56)))
+print("the GCD of 24 and 56 is",str(math.gcd(24, 56)))
