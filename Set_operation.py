e6399bc39a0e7aaa47c10513ac16abfd56fc9ae4
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-06-13 09:03:08 +0200

    Add files via upload

---
+++ Set_operation.py
@@ -0,0 +1,11 @@
+my_set={1, 2, 3, 4}
+print(my_set)
+my_set1={1,0, "hello", (1, 2, 3)}
+print(my_set1)
+my_set2={1, 2, 2, 3, 4, 5, 5, 4}
+print(my_set2)
+my_set3=set([1, 2, 3, 4, 3])
+print(my_set3)
+my_set4={1, 2, 3, 4, 5}
+my_set4.pop()
+print(my_set4)
