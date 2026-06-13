d9650e5066dd15e26c06559098d061ad7e1fd345
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-06-13 16:56:25 +0200

    Add files via upload

---
+++ symentrical_difference.py
@@ -0,0 +1,6 @@
+set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+set2 = {1, 2, 3, 4, 5, 6, 7, 8, 11, 12}
+set3 = set1.symmetric_difference(set2)
+print("Set 1: ", set1)
+print("Set 2: ", set2)
+print("Set 1 & 2 symmetric difference: ", set3)
