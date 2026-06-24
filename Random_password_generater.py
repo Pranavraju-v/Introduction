d27e13bdb33bc5a36a593d59cdb2f01b0b2dbb4f
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-06-24 19:27:47 +0200

    Add files via upload

---
+++ Random_password_generater.py
@@ -0,0 +1,9 @@
+import random as rand
+x = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+random_pass = ''.join(rand.choices(x, k=9))
+print(random_pass) # Example output: gKqZTmAxW
