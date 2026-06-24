f5b579c3f7557daeff165d19443f2bb01839a8d3
Pranavraju-v <sowjanya.venkateshraju@gmail.com>
2026-06-24 19:59:18 +0200

    Add files via upload

---
+++ Class_vehical.py
@@ -0,0 +1,7 @@
+class vehical:
+    def __init__(self, Max_speed, Milage):
+        self.Max_speed=Max_speed
+        self.Milage=Milage
+car=vehical(122, 234)
+print(car.Max_speed)
+print(car.Milage)
